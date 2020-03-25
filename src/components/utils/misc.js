7ca258d93526b0cd2589fc0bb66cba86140bf2d1
bval73 <52051327+bval73@users.noreply.github.com>
2020-03-25 14:30:58 -0400

    final edits for project

--- src/components/utils/misc.js
+++ src/components/utils/misc.js
@@ -9,7 +9,8 @@ export const Tag = (props) => {
                         color: props.color,
                         padding: '5px 10px',
                         display: 'inline-block',
-                        fontFamily: 'Righteous'
+                        fontFamily: 'Righteous',
+                        ...props.add
                       }}
                     >
                       {props.children}
@@ -35,8 +36,8 @@ const Block = (props) => {
                         color: props.color,
                         padding: '5px 10px',
                         display: 'inline-block',
-                        fontFamily: 'Righteous'
-//                        ...props.add
+                        fontFamily: 'Righteous',
+                        ...props.add
                       }}
                     >
                       {props.children}
@@ -51,4 +52,44 @@ const Block = (props) => {
   } else {
     return template;
   }
+}
+
+//loop through entries of firebase entries
+export const firebaseLooper = (snapshot) => {
+  const data = [];
+
+  snapshot.forEach((childSnapshot) => {
+    data.push({
+      ...childSnapshot.val(),
+      id:childSnapshot.key
+    })
+  });
+  return data;
+}
+
+export const reverseArray = (actualArray) => {
+  let reveresedArray = [];
+
+  for(let i=actualArray.length-1; i>-1; i--) {
+    reveresedArray.push(actualArray[i])
+  }
+  return reveresedArray;
+}
+
+export const validate = (element) => {
+  let error = [true, ''];
+
+  if(element.validation.email) {
+    const valid = /\S+@\S+\.\S+/.test(element.value);
+    const message = `${!valid ? 'Must be a valid email' : ''}`;
+    error = !valid ? [valid, message]: error;
+  }
+
+  if(element.validation.required) {
+    const valid = element.value.trim() !== '';
+    const message = `${!valid ? 'This field is required' : ''}`;
+    error = !valid ? [valid, message]: error;
+  }
+
+  return error;
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Resources/css/app.css';
-//import './index.css';
 
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
-//import './firebase';
-//import * as serviceWorker from './serviceWorker';
+import { firebase } from './firebase';
 
-const App = () => {
+const App = (props) => {
   return (
     <BrowserRouter>
-      <Routes />
+      <Routes {...props} />
     </BrowserRouter>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+firebase.auth().onAuthStateChanged((user) => {
+//  console.log(user);
+
+  ReactDOM.render(<App user={user} />, document.getElementById('root'));
+})
+
+
 
 

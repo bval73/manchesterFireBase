ad118928536194cfbe3c8b5d234c8cdfa0cc92e8
bval73 <52051327+bval73@users.noreply.github.com>
2021-03-11 09:50:25 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const config = {
-  apiKey: "AIzaSyDg4h7YTKXXFSGzbo3kkgp87NkbplVjeVo",
-  authDomain: "manchester-26202.firebaseapp.com",
-  databaseURL: "https://manchester-26202.firebaseio.com",
-  projectId: "manchester-26202",
-  storageBucket: "manchester-26202.appspot.com",
-  messagingSenderId: "5165378409",
-  appId: "1:5165378409:web:eb88ab3eec6d9461b72bf8"
+  apiKey: "enter your info",
+  authDomain: "enter your info",
+  databaseURL: "enter your info",
+  projectId: "enter your info",
+  storageBucket: "enter your info",
+  messagingSenderId: "enter your info",
+  appId: "enter your info"
 };
 // Initialize Firebase
 firebase.initializeApp(config);
